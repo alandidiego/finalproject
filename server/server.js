ba666a34487eda3d3a64b064f3f9c6e00a0bcb98
Thunnder <gnorzea@gmail.com>
2022-08-12 06:16:57 -0400

    models&seeds

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express =require('express');
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+db.once('open',()=> {
+    app.listen(PORT, ()=>{
+        console.log(`API server listening on ${PORT}!`);
+    });
+});
