a464c46b51981147c363c5eb6f4877f4d1000276
Thunnder <gnorzea@gmail.com>
2022-08-13 01:04:50 -0400

    connect dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./App.css";
 import Dashboard from "./pages/dashboard";
+// import Footer from "../src/components/Footer"
 
 function App() {
   return (
-    <Dashboard />
+    
+    
+      <Dashboard/>
+    
+    
   )
 }
 
---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer () {
+    return(
+        <section className='footer'>
+               Copyright © 2022 | All Rights Reserved
+               
+        </section>
+    )
+}
+  
+export default Footer;
